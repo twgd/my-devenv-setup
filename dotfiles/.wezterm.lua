9fa6a318b0ab8d0d1a6ed8184589e464e5d2fc86
twgd <twgd.huang@gmail.com>
2025-11-04 18:13:49 +0800

    feat: add key binding for SHIFT+Enter in wezterm configuration

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -30,4 +30,8 @@ config.colors = {
 }
 
 -- and finally, return the configuration to wezterm
+config.keys = {
+  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+}
+
 return config

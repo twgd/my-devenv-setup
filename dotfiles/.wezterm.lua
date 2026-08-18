f1f3d187e6f3e3ba1727148b00cabc8379100ef6
twgd <twgd.huang@gmail.com>
2026-08-18 13:42:46 +0800

    Update terminal font and color scheme

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 17
 
 config.enable_tab_bar = true
 config.tab_max_width = 40
@@ -19,50 +19,7 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
--- my coolnight colorscheme:
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-	tab_bar = {
-		background = "#011423",
-		active_tab = {
-			bg_color = "#033259",
-			fg_color = "#CBE0F0",
-			intensity = "Normal",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-		inactive_tab = {
-			bg_color = "#011423",
-			fg_color = "#CBE0F0",
-			intensity = "Half",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-		inactive_tab_hover = {
-			bg_color = "#033259",
-			fg_color = "#CBE0F0",
-			italic = false,
-		},
-		new_tab = {
-			bg_color = "#011423",
-			fg_color = "#214969",
-		},
-		new_tab_hover = {
-			bg_color = "#033259",
-			fg_color = "#CBE0F0",
-		},
-	},
-}
+config.color_scheme = 'Catppuccin Mocha'
 
 -- and finally, return the configuration to wezterm
 config.keys = {
